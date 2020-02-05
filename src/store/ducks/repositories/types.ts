f6e3f785fe67261e8fb6f7eb5586df8777c51a35
Marcelo Silva <maarcelomonteiro@hotmail.com>
2020-02-05 14:26:10 -0300

    created types from store

---
+++ src/store/ducks/repositories/types.ts
@@ -0,0 +1,30 @@
+/**
+ * Actions types
+ */
+
+export enum RepositoryTypes {
+  LOAD_REQUEST = 'repositories/LOAD_REQUEST',
+  LOAD_SUCCESS = 'repositories/LOAD_SUCCESS',
+  LOAD_FAILURE = 'repositories/LOAD_FAILURE',
+}
+
+/**
+* Data types
+  Formato da informação que ficará dentro do Redux
+*/
+export interface Repository {
+  id: number,
+  name: string,
+}
+
+/**
+* State type
+ Formado do estado do Redux
+*/
+
+
+export interface RepositoryState {
+  readonly data: Repository[],
+  readonly loadging: boolean,
+  readonly error: boolean,
+}
